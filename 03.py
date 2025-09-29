1c97b3edd9e7248171480ee1c4770f4cc4f9575a
Preetam Patel <preetampatel620@gmail.com>
2025-09-29 22:12:48 +0530

    More Functions defined

---
+++ 03.py
@@ -0,0 +1,13 @@
+# name = str(input("Enter your name: "))
+# print(f"Good Afternoon, {name}" )
+
+# name = str(input("Enter your name: "))
+# date = (input("Enter the date (dd-mm-yyyy): "))
+# print(f"Dear {name},\nYour are Selected!\nDate: {date}")
+
+# name = "Preetam is a good  boy"
+# print(name.find("  "))
+
+name = "Preetam is a good  boy"
+print(name.replace("  ", " "))
+print(name) #Strings are immutable
