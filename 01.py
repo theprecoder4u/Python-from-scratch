4df7ce22b45c1a42848e3c0611b7fc11ea9cf338
Preetam Patel <preetampatel620@gmail.com>
2026-01-15 14:40:41 +0530

    test

--- 01.py
+++ 01.py
@@ -1,3 +1,10 @@
-print("Hello World")
-for i in range(1,11):
-    print(5*i)
+# print("Hello World")
+
+
+# for i in range(1,11): 
+    # print(i)
+
+
+a = int(input("TELL ME FIRST NUMBER: "))
+b = int(input("TELL ME Second NUMBER: "))
+print(a-b)
